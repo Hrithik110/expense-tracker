34b0d240fc28fb337a5d4221761abbabc66ef91e
Hrithik110 <Hrithikkumarce2021@gmail.com>
2024-09-10 14:26:31 +0530

    commit

--- src/components/TopExpenses/TopExpenses.jsx
+++ src/components/TopExpenses/TopExpenses.jsx
@@ -1,91 +1,91 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./topExpenses.module.css";
 
 
 
 
-const TopExpenses = ({data})=>{
+const TopExpenses = ({ data }) => {
 
     const [expensesData, setExpensesData] = useState([]);
-    
-   const[total, setTotal] = useState();
+
+    const [total, setTotal] = useState();
 
     useEffect(() => {
         const storedExpensesData = localStorage.getItem('expenses');
         if (storedExpensesData) {
             const expensesArray = JSON.parse(storedExpensesData);
-        const result = Object.values(
-            expensesArray.reduce((acc, item) => {
+            const result = Object.values(
+                expensesArray.reduce((acc, item) => {
 
-                if (!acc[item.category]) {
-                    acc[item.category] = { name: item.category, value: 0 };
-                }
+                    if (!acc[item.category]) {
+                        acc[item.category] = { name: item.category, value: 0 };
+                    }
 
-                acc[item.category].value += item.price;
+                    acc[item.category].value += item.price;
 
-                return acc;
+                    return acc;
+                }, {})
+            )
+            result.sort((a, b) => {
+                return b.value - a.value;
+            })
+            let t = 0;
+            result.reduce((acc, item) => {
+                t += item.value;
             }, {})
-        )
-        result.sort((a,b)=>{
-            return b.value - a.value;
-        })
-        let t = 0;
-        result.reduce((acc, item)=>{
-            t+=item.value;
-        },{})
-        console.log(t);
-        setTotal(t);
-        setExpensesData(result);
+            console.log(t);
+            setTotal(t);
+            setExpensesData(result);
         }
     }, [])
 
- 
-    useEffect(()=>{
+
+    useEffect(() => {
         const storedExpensesData = localStorage.getItem('expenses');
         if (storedExpensesData) {
             const expensesArray = JSON.parse(storedExpensesData);
-        const result = Object.values(
-            expensesArray.reduce((acc, item) => {
+            const result = Object.values(
+                expensesArray.reduce((acc, item) => {
 
-                if (!acc[item.category]) {
-                    acc[item.category] = { name: item.category, value: 0 };
-                }
+                    if (!acc[item.category]) {
+                        acc[item.category] = { name: item.category, value: 0 };
+                    }
 
-                acc[item.category].value += item.price;
+                    acc[item.category].value += item.price;
 
-                return acc;
+                    return acc;
+                }, {})
+            )
+            result.sort((a, b) => {
+                return b.value - a.value;
+            })
+            let t = 0;
+            result.reduce((acc, item) => {
+                t += item.value;
             }, {})
-        )
-        result.sort((a,b)=>{
-            return b.value - a.value;
-        })
-        let t = 0;
-        result.reduce((acc, item)=>{
-            t+=item.value;
-        },{})
-        console.log(t);
-        setTotal(t);
-        setExpensesData(result);
+            console.log(t);
+            setTotal(t);
+            setExpensesData(result);
         }
-    },[data])
+    }, [data])
 
 
-    return(
+    return (
         <div className={styles.topExpensesContainer}>
             <h1>Top Expenses</h1>
 
-            <div className={styles.barContainer}>
+            <table className={styles.barContainer}>
+                <tbody>
+                    {expensesData && expensesData.map((ele, idx) => (
+                        <tr align="right" className={styles.lines} key={idx}>
+                            <td style={{ width: '6rem' }}><h4>{ele.name}</h4></td>
 
-                {expensesData && expensesData.map((ele, idx)=>(
-                    <div className={styles.lines} key={idx}>
-                        <h4>{ele.name}</h4>
-
-                       <hr style={{width:`${Number(ele.value)*10/ total}rem`, height:'1.4rem',borderRadius:'0px 20px 20px 0px', backgroundColor:'#8784D2'}}/>
-                    
-                    </div>
-                ))}
-
-            </div>
+                            <td style={{ width: 'auto' }}> <hr style={{ width: `${Number(ele.value) * 8 / total}rem`, height: '1.4rem', borderRadius: '0px 20px 20px 0px', backgroundColor: '#8784D2' }} />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
 
         </div>
 
